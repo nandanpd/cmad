e6bfde6ea3bffe6f498ca514667d7ef92e37b774
Nandan Devarajulu <mailtonandan@gmail.com>
2017-05-12 13:53:52 +0530

    test commit for unit testing

---
+++ src/test/java/com/glarimy/vertx/HelloTestCase.java
@@ -0,0 +1,14 @@
+package com.glarimy.vertx;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloTestCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
